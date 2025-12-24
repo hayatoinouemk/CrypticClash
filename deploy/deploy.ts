3a5e91250e266e795a415d42ebc07e6e0b8d7db8
hayatoinouemk <yszjka0533@outlook.com>
2025-12-24 13:12:38 +0800

    Tidy Hardhat config and deploy script

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,13 +5,13 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const deployedCrypticClash = await deploy("CrypticClash", {
     from: deployer,
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  console.log(`CrypticClash contract: `, deployedCrypticClash.address);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_cryptic_clash"; // id required to prevent reexecution
+func.tags = ["CrypticClash"];
